a67c8af888eccae1b45d871f2cb4cfe747ace128
Beauty kumari <kbeauty193@gmail.com>
2024-07-29 21:01:56 +0530

    push

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 function App() {
   return (
   <>
 <Navbar />
+<Banner />
 
 </>
   )
--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -3,9 +3,12 @@ import React from "react";
 
 function Banner() {
   return (
-    <div>
-      
-    </div>
+   <>
+<div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+<div>Left</div>
+<div>Right</div>
+</div>
+</>
   )
 }
 
