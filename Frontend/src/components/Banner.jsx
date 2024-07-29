0afdb6cce1c473959779f7f322c3e46e460e44c0
Beauty kumari <kbeauty193@gmail.com>
2024-07-29 21:26:01 +0530

    add

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -3,13 +3,38 @@ import React from "react";
 
 function Banner() {
   return (
-   <>
-<div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-<div>Left</div>
-<div>Right</div>
-</div>
-</>
-  )
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+          <div className="space-y-12">
+            <h1 className="text-4xl font-bold">
+              Hello, Welcome here to learn something{" "}
+              <span className="text-pink-500">new everyday!!!</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus illum officia cum earum unde voluptatem hic, repellat,
+              expedita consectetur animi eaque ullam deleniti odit veniam quas?
+              Pariatur doloremque corporis aspernatur.
+            </p>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Email" />
+            </label>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">Right</div>
+      </div>
+    </>
+  );
 }
 
-export default Banner
+export default Banner;
