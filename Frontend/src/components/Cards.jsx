d40ed8940a06d7944a510b6a50a3f7bacccab52f
TrickyVikram <vkumarsah999@gmail.com>
2024-08-06 00:02:42 +0530

    Update

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,18 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-function Cards({ item }) {
+// eslint-disable-next-line react/prop-types
+function Cards({ name, image, title, category }) {
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
-        <img src={item.image} alt="Shoes" />
+        <img src={image} alt={name} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          {item.name}
-          <div className="badge badge-secondary">{item.category}</div>
+          {name}
+          <div className="badge badge-secondary">  {category}</div>
         </h2>
-        <p>{item.title}</p>
+        <p>Title: {title}</p>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
           <div className="badge badge-outline">Products</div>
