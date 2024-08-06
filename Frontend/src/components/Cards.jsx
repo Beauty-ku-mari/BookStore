bb771a99df4909c6f7c50747c5ad9d93aa880eb3
Beauty kumari <kbeauty193@gmail.com>
2024-08-07 02:29:52 +0530

    update

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -4,7 +4,9 @@ import React from "react";
 // eslint-disable-next-line react/prop-types
 function Cards({ name, image, title, price, category }) {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+<>
+<div className=" mt-4 my-3 p-3">
+    <div className="card w bg-base-100 shadow-xl">
       <figure>
         <img src={image} alt={name} />
       </figure>
@@ -16,10 +18,12 @@ function Cards({ name, image, title, price, category }) {
         <p>Title: {title}</p>
         <div className="card-actions justify-between">
           <div className="badge badge-outline">${price}</div>
-          <div className="badge badge-outline">Buy Now</div>
+          <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">Buy Now</div>
         </div>
       </div>
     </div>
+</div>
+</>
   );
 }
 
