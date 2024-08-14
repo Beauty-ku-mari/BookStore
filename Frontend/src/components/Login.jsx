2372fd77c2299d8daa7359ee800eb9d1a4aab59c
Beauty kumari <kbeauty193@gmail.com>
2024-08-14 17:15:49 +0530

    login

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
 function Login() {
   return (
     <div>
-      
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Login</h3>
+          {/* { Email } */}
+          <div className="mt-4 space-y-2">
+            <span>Email</span>
+            <br />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-80 px-3 py-1 border rounded-md outline-none "
+            />
+          </div>
+          {/* { Password }  */}
+          <div className="mt-4 space-y-2">
+            <span>Password</span>
+            <br />
+            <input
+              type="text"
+              placeholder="Enter your password"
+              className="w-80 px-3 py-1 border rounded-md outline-none "
+            />
+          </div>
+          {/* {   Button } */}
+          <div className="flex justify-around mt-4">
+            <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">Login</button>
+            <p>
+              Not registered? <span className=" underline text-blue-500" >Signup</span>{" "}
+            </p>
+          </div>
+        </div>
+      </dialog>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
