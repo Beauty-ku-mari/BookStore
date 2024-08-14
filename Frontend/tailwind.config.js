084f3ed9145f284dc6cb1780b20beec2b49ef897
Beauty kumari <kbeauty193@gmail.com>
2024-08-14 14:56:52 +0530

    theme

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    "./src/**/*.{js,ts,jsx,tsx}"],
+ darkMode: 'class',
   theme: {
     extend: {},
   },
