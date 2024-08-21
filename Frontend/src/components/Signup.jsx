21aa6c7c4b966b692fbde3813ba99252a3e7d90c
Beauty kumari <kbeauty193@gmail.com>
2024-08-21 11:17:43 +0530

    'push'

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-undef */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Link } from "react-router-dom";
@@ -5,15 +6,28 @@ function Signup() {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
-        <div id="my_modal_3" className="border-[2px] shadow-md p-5 rounded-md">
-          <div className="">
-            <form method="div">
+        <div id="my_modal_3" className="">
+          <div className="modal-box">
+            <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              <Link
+                to="/"
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              >
                 ✕
-              </button>
+              </Link>
             </form>
             <h3 className="font-bold text-lg">Signup</h3>
+            {/* {  name    } */}
+            <div className="mt-4 space-y-2">
+              <span>Name</span>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter your full name"
+                className="w-80 px-3 py-1 border rounded-md outline-none "
+              />
+            </div>
             {/* { Email } */}
             <div className="mt-4 space-y-2">
               <span>Email</span>
@@ -37,16 +51,20 @@ function Signup() {
             {/* {   Button } */}
             <div className="flex justify-around mt-4">
               <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-                Login
+                Signup
               </button>
               <p>
-                Not registered?{" "}
-                <Link
-                  to="/signup"
+                Have account?{" "}
+                <button
+                  to="/"
                   className=" underline text-blue-500 cursor-pointer"
+                  onClick={() =>
+                    document.getElementById("my_modal_3").showModal()
+                  }
                 >
-                  Signup
-                </Link>{" "}
+                  login
+                </button>{" "}
+              
               </p>
             </div>
           </div>
