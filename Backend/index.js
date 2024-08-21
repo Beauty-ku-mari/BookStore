bf8e04f69a12ea7ea97f4935d8d3217c4b0f94c2
Beauty kumari <kbeauty193@gmail.com>
2024-08-21 23:02:29 +0530

    data

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const dotenv = require("dotenv");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
 
-app.get('/', (req, res) => {
-    res.send('book store app')
-});
+// connect to mongoDB
+try{
+    mongoose.connect(URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        });
+        console.log("Connected to mongoDB");
+} catch(error){
+     console.log("Error: ",error)
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
