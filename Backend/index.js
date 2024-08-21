5eef51196b4ddf1cb5d1c147265e5bba3142598d
Beauty kumari <kbeauty193@gmail.com>
2024-08-21 22:24:45 +0530

    'update'

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('book store app')
+    res.send('book store app')
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
