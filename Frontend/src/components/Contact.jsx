5bac29626336753a3330373193b8ed5b72eb7be0
Beauty kumari <kbeauty193@gmail.com>
2024-08-23 15:40:24 +0530

    add

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -54,8 +54,12 @@ Name
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Your Email"
           />
+</div>
 
-
+<div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Message
+          </label>
 
 </form>
 
