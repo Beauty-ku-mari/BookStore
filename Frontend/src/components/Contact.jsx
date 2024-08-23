bb56dfc88092308822edcf1396813273f1995075
Beauty kumari <kbeauty193@gmail.com>
2024-08-23 15:20:34 +0530

    add

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -28,9 +28,17 @@ return(
 
 <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-
+Name
 </label>
-
+<input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Your Name"
+          />
 
 
 
