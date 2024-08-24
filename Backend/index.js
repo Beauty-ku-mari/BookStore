6fb54d31d8ab8f2304ab25b0e98d888a023712aa
Beauty kumari <kbeauty193@gmail.com>
2024-08-25 03:30:05 +0530

    'commit'

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
 
 const app = express();
 
@@ -27,6 +28,7 @@ try{
 
 // defining  routes
 app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
