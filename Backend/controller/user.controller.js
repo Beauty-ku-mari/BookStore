992f8f188f71f2e416b5f8801d1a0ec19e641418
Beauty kumari <kbeauty193@gmail.com>
2024-08-25 03:22:52 +0530

    'commit'

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -15,7 +15,8 @@ export const signup = async (req, res) => {
         });
         createUser.save();
         res.status(201).json({ message: "Internal server error" });
-    } catch {
+    } catch(error) {
         console.log("Error: " + error.message);
+res.status(500).json({ message: "Internal server error" });
     }
 };
