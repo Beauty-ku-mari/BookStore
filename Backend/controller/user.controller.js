3f2b4f1103693a51ca69f97f0a1bb26dceeda52c
Beauty kumari <kbeauty193@gmail.com>
2024-08-25 04:44:53 +0530

    'commit'

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -29,11 +29,16 @@ export const login = async (req, res) => {
         const user = await User.findOne({ email });
         const isMatch = await bcryptjs.compare(password, user.password);
         if (!user || !isMatch) {
-
-
+            return res.status(400).json({ message: "Invalid username or password" });
         } else {
-
-
+            res.status(200).json({
+                message: "Login successful",
+                user: {
+                    _id: user._id,
+                    fullname: user.fullname,
+                    email: user.email,
+                },
+            });
         }
     } catch {
 
