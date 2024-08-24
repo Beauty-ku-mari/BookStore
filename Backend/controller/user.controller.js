0a29695d89c72cc4a8fa3a5ebbc7a49974de391d
Beauty kumari <kbeauty193@gmail.com>
2024-08-25 03:06:41 +0530

    'commit'

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -2,7 +2,8 @@ import User from "../model/user.model.js";
 
 export const signup = async(req, res) => {
 try{
-
+const { fullname, email, password } = req.body;
+        const user = await User.findOne({ email });
 
 
 }catch{
