e06e8a434deb0241067ef1832c8306e89d59195c
Beauty kumari <kbeauty193@gmail.com>
2024-08-25 03:09:13 +0530

    'commit'

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,15 +1,17 @@
 import User from "../model/user.model.js";
 
-export const signup = async(req, res) => {
-try{
-const { fullname, email, password } = req.body;
+export const signup = async (req, res) => {
+    try {
+        const { fullname, email, password } = req.body;
         const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+
+    } catch {
 
 
-}catch{
 
 
-
-
-}
+    }
 };
