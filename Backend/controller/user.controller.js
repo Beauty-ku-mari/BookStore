9fb97a349ee75e69f2f111fa05da0561d1439fdc
Beauty kumari <kbeauty193@gmail.com>
2024-08-25 10:39:44 +0530

    commit

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -16,7 +16,14 @@ export const signup = async (req, res) => {
             password: hashPassword,
         });
         await createdUser.save();
-        res.status(201).json({ message: "User created successfully" });
+        res.status(201).json({
+            message: "User created successfully",
+            user: {
+                _id: createdUser._id,
+                fullname: createdUser.fullname,
+                email: createdUser.email,
+},
+        });
     } catch (error) {
         console.log("Error: " + error.message);
         res.status(500).json({ message: "Internal server error" });
